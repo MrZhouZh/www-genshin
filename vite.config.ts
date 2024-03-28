3ad73b3c8986931a182e7c8862bb48bd9a9580b7
zhouzhikai <zhouzhikai@smm.cn>
2024-03-28 14:51:21 +0800

    chore: add splitVendorChunkPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,34 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
+import { dependencies } from './package.json'
+
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {}
+  Object.keys(deps).forEach(key => {
+    if (['react', 'react-dom'].includes(key)) return
+    chunks[key] = [key]
+  })
+
+  return chunks
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ...renderChunks(dependencies)
+        }
+      }
     }
   }
 })
